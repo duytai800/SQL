a2ab47b76cc2516332ca132575bbbf02ab8fc337
duytai800 <77057189+duytai800@users.noreply.github.com>
2021-11-29 15:48:53 +0700

    Assignments

---
+++ UIT/20520299_QuanLyBanHang.sql
@@ -0,0 +1,356 @@
+﻿------------------------------------------QUAN LY BAN HANG------------------------------------------
+--1. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE NUOCSX='Trung Quoc'
+
+--2. In ra danh sách các sản phẩm (MASP, TENSP) có đơn vị tính là “cay”, ”quyen”.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE DVT IN('cay', 'quyen')
+
+--3. In ra danh sách các sản phẩm (MASP,TENSP) có mã sản phẩm bắt đầu là “B” và kết 
+--thúc là “01”.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE MASP LIKE 'B%1'
+
+--4. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quốc” sản xuất có giá từ 30.000 
+--đến 40.000.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE NUOCSX='Trung Quoc' and (gia between 30000 and 40000)
+
+--5. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” hoặc “Thai Lan” sản 
+--xuất có giá từ 30.000 đến 40.000.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE (NUOCSX='Trung Quoc' or NUOCSX='Thai Lan') and (gia between 30000 and 40000)
+
+--6. In ra các số hóa đơn, trị giá hóa đơn bán ra trong ngày 1/1/2007 và ngày 2/1/2007.
+SELECT SOHD, TRIGIA
+FROM HOADON
+WHERE NGHD BETWEEN '1/1/2007' AND '2/1/2007'
+
+--7. In ra các số hóa đơn, trị giá hóa đơn trong tháng 1/2007, sắp xếp theo ngày (tăng dần) và 
+--trị giá của hóa đơn (giảm dần).
+SELECT SOHD, TRIGIA
+FROM HOADON
+WHERE MONTH(NGHD)=7 AND YEAR(NGHD)=2007
+ORDER BY NGHD ASC, TRIGIA DESC
+
+--8. In ra danh sách các khách hàng (MAKH, HOTEN) đã mua hàng trong ngày 1/1/2007.
+SELECT KH.MAKH, HOTEN 
+FROM KHACHHANG KH JOIN HOADON HD ON KH.MAKH=HD.MAKH
+WHERE NGHD ='1/1/2007'
+ 
+--9. In ra số hóa đơn, trị giá các hóa đơn do nhân viên có tên “Nguyen Van B” lập trong ngày 
+--28/10/2006.
+SET DATEFORMAT DMY
+SELECT SOHD, TRIGIA
+FROM NHANVIEN NV JOIN HOADON HD ON NV.MANV= HD.MANV
+WHERE NGHD='28/10/2006' AND HOTEN='Nguyen Van B'
+
+--10. In ra danh sách các sản phẩm (MASP,TENSP) được khách hàng có tên “Nguyen Van A” 
+--mua trong tháng 10/2006.
+SELECT SP.MASP, TENSP
+FROM KHACHHANG KH, SANPHAM SP,CTHD, HOADON HD
+WHERE KH.MAKH=HD.MAKH AND HD.SOHD=CTHD.SOHD AND CTHD.MASP=SP.MASP AND
+		MONTH(NGHD)='10' AND YEAR(NGHD)='2006' AND HOTEN='Nguyen Van A'
+
+--11. Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”.
+SELECT distinct hd.SOHD
+FROM HOADON hd, CTHD ct
+WHERE hd.SOHD = ct.SOHD AND ct.MASP IN ('BB01','BB02')
+
+--12. Tìm các số óa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”, mỗi sản phẩm 
+--mua với số lượng từ 10 đến 20.
+SELECT distinct hd.SOHD
+FROM HOADON hd, CTHD ct
+WHERE hd.SOHD = ct.SOHD AND ct.MASP IN ('BB01','BB02') AND ct.SL BETWEEN 10 AND 20
+
+--13. Tìm các số hóa đơn mua cùng lúc 2 sản phẩm có mã số “BB01” và “BB02”, mỗi sản 
+--phẩm mua với số lượng từ 10 đến 20.
+SELECT SOHD
+FROM CTHD
+WHERE MASP = 'BB01' AND SL BETWEEN 10 AND 20
+INTERSECT
+(
+SELECT SOHD
+FROM CTHD
+WHERE MASP = 'BB02' AND SL BETWEEN 10 AND 20
+)
+
+--14. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất hoặc các sản 
+--phẩm được bán ra trong ngày 1/1/2007.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE NUOCSX='TRUNG QUOC'
+UNION
+SELECT SP.MASP, SP.TENSP
+FROM HOADON AS HD, SANPHAM AS SP, CTHD AS CT
+WHERE HD.NGHD='1/1/2007'
+		AND SP.MASP=CT.MASP
+		AND HD.SOHD=CT.SOHD
+
+--15. In ra danh sách các sản phẩm (MASP,TENSP) không bán được.
+SELECT MASP, TENSP
+FROM SANPHAM SP
+WHERE SP.MASP NOT IN ( SELECT MASP
+						FROM CTHD CT
+						WHERE CT.MASP= SP.MASP)
+
+--16. In ra danh sách các sản phẩm (MASP,TENSP) không bán được trong năm 2006.
+SELECT MASP, TENSP
+FROM SANPHAM SP
+WHERE SP.MASP NOT IN ( SELECT MASP
+						FROM CTHD CT, HOADON HD
+						WHERE CT.MASP= SP.MASP AND YEAR(HD.NGHD)='2006'
+								AND SP.MASP=CT.MASP
+								AND HD.SOHD=CT.SOHD )
+
+--17. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất không bán 
+--được trong năm 2006.
+SELECT MASP, TENSP
+FROM SANPHAM SP
+WHERE SP.NUOCSX='Trung Quoc'
+	AND SP.MASP NOT IN (   SELECT MASP 
+						FROM CTHD CT, HOADON HD
+						WHERE CT.MASP=SP.MASP AND  YEAR(HD.NGHD)='2006'
+						AND SP.MASP=CT.MASP
+						AND HD.SOHD=CT.SOHD )
+
+--18. Tìm số hóa đơn đã mua tất cả các sản phẩm do Singapore sản xuất.
+SELECT SOHD
+FROM HOADON
+WHERE
+NOT EXISTS
+(
+    SELECT *
+    FROM SANPHAM
+    WHERE NUOCSX= 'Singapore' AND MASP NOT IN
+    (
+        SELECT masp
+        FROM CTHD
+        WHERE SOHD = HOADON.SOHD and CTHD.MASP = SANPHAM.MASP
+    )
+)
+
+--19. Tìm số hóa đơn trong năm 2006 đã mua ít nhất tất cả các sản phẩm do Singapore sản 
+--xuất
+SELECT SOHD
+FROM HOADON
+WHERE YEAR( HOADON.NGHD)='2006' AND
+NOT EXISTS
+(
+    SELECT *
+    FROM SANPHAM
+    WHERE NUOCSX= 'Singapore' AND MASP NOT IN
+    (
+        SELECT masp
+        FROM CTHD
+        WHERE SOHD = HOADON.SOHD and CTHD.MASP = SANPHAM.MASP
+    )
+)
+
+--20. Có bao nhiêu hóa đơn không phải của khách hàng đăng ký thành viên mua?SELECT count(SOHD) as SL
+FROM HOADON
+WHERE MAKH is null
+
+--21. Có bao nhiêu sản phẩm khác nhau được bán ra trong năm 2006.
+SELECT count(distinct ct.MASP)
+FROM HOADON hd, CTHD ct
+WHERE YEAR(hd.NGHD) = 2006 and ct.SOHD=hd.SOHD
+
+--22. Cho biết trị giá hóa đơn cao nhất, thấp nhất là bao nhiêu ?
+ SELECT MAX(TRIGIA) MAXTG, MIN(TRIGIA) MINTG
+FROM HOADON;
+
+--23. Trị giá trung bình của tất cả các hóa đơn được bán ra trong năm 2006 là bao nhiêu?
+SELECT AVG(TRIGIA) 
+FROM HOADON
+WHERE YEAR(NGHD)='2006'
+
+--24. Tính doanh thu bán hàng trong năm 2006.
+SELECT SUM(TRIGIA) 'DoanhThu'
+FROM HOADON
+WHERE YEAR(NGHD)='2006'
+
+--25. Tìm số hóa đơn có trị giá cao nhất trong năm 2006.
+SELECT SOHD
+FROM HOADON
+WHERE YEAR (NGHD)='2006' AND TRIGIA IN (
+SELECT max(hd.TRIGIA)
+FROM HOADON hd
+WHERE YEAR(hd.NGHD) = 2006
+)
+
+--26. Tìm họ tên khách hàng đã mua hóa đơn có trị giá cao nhất trong năm 2006.
+SELECT HOTEN
+FROM KHACHHANG KH JOIN HOADON ON KH.MAKH=HOADON.MAKH
+WHERE YEAR (NGHD)='2006' AND TRIGIA IN (
+SELECT max(hd.TRIGIA)
+FROM HOADON hd
+WHERE YEAR(hd.NGHD) = 2006
+)
+--27. In ra danh sách 3 khách hàng đầu tiên (MAKH, HOTEN) sắp xếp theo doanh số giảm 
+--dần.
+SELECT TOP 3 MAKH, HOTEN
+FROM KHACHHANG
+ORDER BY DOANHDO DESC
+
+--28. In ra danh sách các sản phẩm (MASP, TENSP) có giá bán bằng 1 trong 3 mức giá cao 
+--nhất.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE GIA IN (	SELECT DISTINCT TOP 3 GIA
+				FROM SANPHAM
+				ORDER BY GIA DESC)
+
+--29. In ra danh sách các sản phẩm (MASP, TENSP) do “Thai Lan” sản xuất có giá bằng 1 
+--trong 3 mức giá cao nhất (của tất cả các sản phẩm).
+SELECT SANPHAM.MASP, SANPHAM.TENSP
+FROM SANPHAM
+WHERE SANPHAM.NUOCSX='Thai Lan' 
+AND
+SANPHAM.GIA IN
+(
+SELECT DISTINCT TOP 3 sp.GIA
+FROM SANPHAM sp
+ORDER BY sp.GIA DESC
+)
+
+--30. In ra danh sách các sản phẩm (MASP, TENSP) do “Trung Quoc” sản xuất có giá bằng 1 
+--trong 3 mức giá cao nhất (của sản phẩm do “Trung Quoc” sản xuất).
+SELECT SANPHAM.MASP, SANPHAM.TENSP
+FROM SANPHAM
+WHERE SANPHAM.NUOCSX='Trung Quoc' 
+AND
+SANPHAM.GIA IN
+(
+SELECT DISTINCT TOP 3 sp.GIA
+FROM SANPHAM sp
+WHERE sp.NUOCSX = 'Trung Quoc'
+ORDER BY sp.GIA DESC
+)
+
+--31. In ra danh sách 3 khách hàng có doanh số cao nhất (sắp xếp theo kiểu xếp hạng).
+SELECT TOP 3 MAKH, HOTEN
+FROM KHACHHANG
+ORDER BY DOANHSO DESC
+
+--32. Tính tổng số sản phẩm do “Trung Quoc” sản xuất.
+SELECT COUNT(DISTINCT MASP)
+FROM SANPHAM
+WHERE NUOCSX = 'TRUNG QUOC'
+
+--33. Tính tổng số sản phẩm của từng nước sản xuất.
+SELECT NUOCSX, COUNT(DISTINCT MASP) AS TONGSOSANPHAM
+FROM SANPHAM
+GROUP BY NUOCSX
+
+--34. Với từng nước sản xuất, tìm giá bán cao nhất, thấp nhất, trung bình của các sản phẩm.
+SELECT NUOCSX, MAX(GIA) AS MAX, MIN(GIA) AS MIN, AVG(GIA) AS AVG
+FROM SANPHAM
+GROUP BY NUOCSX
+
+--35. Tính doanh thu bán hàng mỗi ngày.
+SELECT NGHD, SUM(TRIGIA) AS DOANHTHU
+FROM HOADON
+GROUP BY NGHD
+
+--36. Tính tổng số lượng của từng sản phẩm bán ra trong tháng 10/2006.
+SELECT MASP, COUNT(DISTINCT MASP) AS TONGSO
+FROM SANPHAM
+WHERE MASP IN(SELECT MASP
+FROM CTHD C INNER JOIN HOADON H
+ON C.SOHD = H.SOHD
+WHERE MONTH(NGHD) = 10 AND YEAR(NGHD) = 2006)
+GROUP BY MASP
+
+--37. Tính doanh thu bán hàng của từng tháng trong năm 2006.
+SELECT MONTH(NGHD) AS THANG, SUM(TRIGIA) AS DOANHTHU
+FROM HOADON
+WHERE YEAR(NGHD) = 2006
+GROUP BY MONTH(NGHD)
+
+--38. Tìm hóa đơn có mua ít nhất 4 sản phẩm khác nhau.
+SELECT *
+FROM HOADON
+WHERE SOHD IN(SELECT SOHD
+FROM CTHD
+WHERE SL >= 4)
+
+--39. Tìm hóa đơn có mua 3 sản phẩm do “Viet Nam” sản xuất (3 sản phẩm khác nhau).
+SELECT *
+FROM HOADON
+WHERE SOHD IN(SELECT SOHD
+FROM CTHD C INNER JOIN SANPHAM S
+ON C.MASP = S.MASP
+WHERE NUOCSX = 'VIET NAM' AND SL >= 3)
+
+--40. Tìm khách hàng (MAKH, HOTEN) có số lần mua hàng nhiều nhất. 
+SELECT MAKH, HOTEN
+FROM KHACHHANG
+WHERE MAKH = (SELECT TOP 1 MAKH
+FROM HOADON
+GROUP BY MAKH
+ORDER BY COUNT(DISTINCT SOHD) DESC)
+
+--41. Tháng mấy trong năm 2006, doanh số bán hàng cao nhất ?
+SELECT TOP 1 MONTH(NGHD) AS THANG_DOANHSO_MAX
+FROM HOADON
+WHERE YEAR(NGHD) = 2006
+GROUP BY MONTH(NGHD)
+ORDER BY SUM(TRIGIA) DESC
+
+--42. Tìm sản phẩm (MASP, TENSP) có tổng số lượng bán ra thấp nhất trong năm 2006.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE MASP = (SELECT TOP 1 MASP
+FROM CTHD
+GROUP BY MASP
+ORDER BY SUM(SL) DESC)
+
+--43. *Mỗi nước sản xuất, tìm sản phẩm (MASP,TENSP) có giá bán cao nhất.
+SELECT sp1.NUOCSX, sp1.MASP, sp1.TENSP
+FROM SANPHAM sp1
+WHERE sp1.GIA in
+(
+    SELECT max(sp.GIA)
+    FROM SANPHAM sp
+    WHERE sp1.NUOCSX = sp.NUOCSX
+)
+
+--44. Tìm nước sản xuất sản xuất ít nhất 3 sản phẩm có giá bán khác nhau.
+SELECT sp.NUOCSX
+FROM SANPHAM sp
+GROUP BY sp.NUOCSX
+HAVING count(distinct sp.GIA)>=3
+
+--45. *Trong 10 khách hàng có doanh số cao nhất, tìm khách hàng có số lần mua hàng nhiều nhất.
+SELECT hd1.MAKH, DS1.HOTEN
+FROM 
+(
+    SELECT TOP 10 kh1.MAKH, KH1.HOTEN
+    FROM KHACHHANG kh1
+    WHERE kh1.MAKH is not null
+    ORDER BY kh1.DOANHSO DESC
+) DS1, HOADON hd1
+WHERE DS1.MAKH = hd1.MAKH
+GROUP BY hd1.MAKH, DS1.HOTEN
+ 
+HAVING COUNT(HD1.SOHD)>=
+ALL(
+ 
+    SELECT count(hd.SOHD)
+    FROM 
+    (
+        SELECT TOP 10 kh.MAKH
+        FROM KHACHHANG kh
+        WHERE kh.MAKH is not null
+        ORDER BY kh.DOANHSO DESC
+    ) DS, HOADON hd
+    WHERE DS.MAKH = hd.MAKH
+    GROUP BY hd.MAKH 
+)
---
+++ UIT/20520299_QuanLyHangHoa.sql
@@ -0,0 +1,229 @@
+﻿------------------------------------------QUAN LY HANG HOA------------------------------------------
+--1. Hiển thị thông tin (maNCC, tenNCC, thanhpho) của tất cả nhà cung cấp.
+SELECT maNCC, tenNCC, thanhpho
+FROM Nhacungcap
+
+--2. Hiển thị thông tin của tất cả các phụ tùng.
+SELECT maPT, tenPT, mausac, khoiluong, thanhpho
+FROM Phutung
+
+--3. Hiển thị thông tin các nhà cung cấp ở thành phố London.
+SELECT maNCC, tenNCC,trangthai, thanhpho
+FROM Nhacungcap
+WHERE thanhpho='London';
+
+--4. Hiển thị mã phụ tùng, tên và màu sắc của tất cả các phụ tùng ở thành 
+--phố Paris.
+SELECT maPT, tenPT, mausac
+FROM Phutung
+WHERE thanhpho='Paris';
+
+--5. Hiển thị mã phụ tùng, tên, khối lượng của những phụ tùng có khối 
+--lượng lớn hơn 15.
+SELECT maPT, tenPT, khoiluong
+FROM Phutung
+WHERE khoiluong>15;
+
+--6. Tìm những phụ tùng (maPT, tenPt, mausac) có khối lượng lớn hơn 15, 
+--không phải màu đỏ (red).
+SELECT maPT, tenPT, mausac
+FROM Phutung
+WHERE khoiluong >15 and mausac != 'red';
+
+--7. Tìm những phụ tùng (maPT, tenPt, mausac) có khối lượng lớn hơn 15, 
+--màu sắc khác màu đỏ (red) và xanh (green).
+SELECT maPT, tenPt, mausac
+FROM Phutung
+WHERE khoiluong >15 and mausac not in  ('red', 'green')
+
+--8. Hiển thị những phụ tùng (maPT, tenPT, khối lượng) có khối lượng lớn 
+--hơn 15 và nhỏ hơn 20, sắp xếp theo tên phụ tùng.
+SELECT maPT, tenPt, khoiluong
+FROM Phutung
+WHERE khoiluong>15 and khoiluong <20 
+ORDER BY tenPT ASC
+
+--9. Hiển thị những phụ tùng được vận chuyển bởi nhà cung cấp có mã số S1. 
+--Không hiển thị kết quả trùng. (sử dụng phép kết).
+SELECT PT.MAPT, TENPT
+FROM PHUTUNG PT JOIN Vanchuyen VC ON PT.MAPT=VC.MAPT
+WHERE maNCC='S1' 
+
+--10. Hiển thị những nhà cung cấp vận chuyển phụ tùng có mã là P1 (sử dụng 
+--phép kết).
+select *
+from Nhacungcap n join Vanchuyen vc
+on n.maNCC = vc.maNCC
+where vc.maPT = 'P1'
+
+--11. Hiển thị thông tin nhà cung cấp ở thành phố London và có vận chuyển 
+--phụ tùng của thành phố London. Không hiển thị kết quả trùng. (Sử dụng 
+--phép kết)
+select n.maNCC, n.tenNCC
+from (Nhacungcap n join Vanchuyen vc on n.maNCC = vc.maNCC) join Phutung pt on vc.maPT = pt.maPT
+where n.maNCC = 'London' and pt.thanhpho = 'London'
+
+--12. Lặp lại câu 9 nhưng sử dụng toán tử IN.
+--Hiển thị những phụ tùng được vận chuyển bởi nhà cung cấp có mã số S1. 
+--Không hiển thị kết quả trùng
+SELECT MAPT, TENPT
+FROM PHUTUNG
+WHERE MAPT IN (SELECT MAPT
+			   FROM VANCHUYEN VC
+			   WHERE VC.MAPT =PHUTUNG.MAPT AND MANCC='S1');
+
+--13. Lặp lại câu 10 nhưng sử dụng toán tử IN  
+-- Hiển thị những nhà cung cấp vận chuyển phụ tùng có mã là P1 (sử dụng 
+--phép kết
+select *
+FROM Nhacungcap NCC
+WHERE MANCC IN ( SELECT MANCC
+				FROM VANCHUYEN VC
+				WHERE NCC.maNCC= VC.maNCC AND VC.MAPT='P1')
+
+--14. Lặp lại câu 9 nhưng sử dụng toán tử EXISTS
+SELECT MAPT, TENPT
+FROM PHUTUNG
+WHERE EXISTS (	SELECT *
+				FROM VANCHUYEN VC
+				WHERE VC.MAPT=Phutung.MAPT AND MANCC='S1');
+
+--15. Lặp lại câu 10 nhưng sử dụng toán tử EXISTS 
+--Hiển thị những nhà cung cấp vận chuyển phụ tùng có mã là P1 (sử dụng 
+--phép kết)
+SELECT *
+FROM NHACUNGCAP NCC
+WHERE EXISTS (  SELECT *
+				FROM Vanchuyen VC
+				WHERE NCC.maNCC= VC.maNCC AND VC.MAPT='P1')
+
+--16. Lặp lại câu 11 nhưng sử dụng truy vấn con. Sử dụng toán tử IN.
+--Hiển thị thông tin nhà cung cấp ở thành phố London và có vận chuyển 
+--phụ tùng của thành phố London. Không hiển thị kết quả trùng.
+SELECT *
+FROM Nhacungcap ncc
+WHERE maNCC IN (SELECT maNCC
+FROM Vanchuyen vc JOIN Phutung pt on vc.maPT=pt.maPT
+WHERE ncc.thanhpho='London' and pt.thanhpho = 'London' and ncc.maNCC=vc.maNCC)
+
+--17. Lặp lại câu 11 nhưng dùng truy vấn con. Sử dụng toán tử EXISTS. 
+SELECT *
+FROM Nhacungcap ncc
+WHERE exists (SELECT *
+FROM Vanchuyen vc JOIN Phutung pt on vc.maPT=pt.maPT
+WHERE ncc.thanhpho='London' and pt.thanhpho = 'London' and ncc.maNCC=vc.maNCC)
+
+--18. Tìm nhà cung cấp chưa vận chuyển bất kỳ phụ tùng nào. Sử dụng NOT 
+--IN.
+SELECT *
+FROM Nhacungcap
+WHERE maNCC NOT IN(SELECT VC.maNCC
+FROM Vanchuyen VC
+WHERE VC.maNCC=Nhacungcap.maNCC );
+
+--19. Tìm nhà cung cấp chưa vận chuyển bất kỳ phụ tùng nào. Sử dụng NOT 
+--EXISTS.
+select *
+from Nhacungcap 
+where not exists (select maNCC from Vanchuyen
+where Nhacungcap.maNCC = Vanchuyen.maNCC)
+
+--20. Tìm nhà cung cấp chưa vận chuyển bất kỳ phụ tùng nào. Sử dụng outer 
+--JOIN (Phép kết ngoài)
+select *
+from Nhacungcap n left join Vanchuyen v
+on n.maNCC = v.maNCC
+where v.maPT is null
+
+--21. Có tất cả bao nhiêu nhà cung cấp?
+SELECT COUNT(MANCC) 'SL_NCC'
+FROM NHACUNGCAP
+
+--22. Có tất cả bao nhiêu nhà cung cấp ở London?
+SELECT COUNT(MANCC) 
+FROM NHACUNGCAP
+WHERE thanhpho='London'
+--23. Hiển thị trị giá cao nhất, thấp nhất của trangthai của các nhà cung 
+--cấp.
+SELECT MAX(TRANGTHAI) TrangThaiCaoNhat, Min(Trangthai) TrangThaiThapNhat
+FROM Nhacungcap
+
+--24. Hiển thị giá trị cao nhất, thấp nhất của trangthai trong table 
+--nhacungcap ở thành phố London.
+SELECT MAX(TRANGTHAI) TrangThaiCaoNhat, Min(Trangthai) TrangThaiThapNhat
+FROM Nhacungcap
+WHERE thanhpho='London'
+
+--25. Mỗi nhà cung cấp vận chuyển bao nhiêu phụ tùng? Chỉ hiển thị mã nhà
+--cung cấp, tổng số phụ tùng đã vận chuyển.
+SELECT NCC.maNCC, SUM(soluong) 'SL_PT'
+FROM Vanchuyen VC FULL JOIN Nhacungcap NCC ON VC.MANCC=NCC.MANCC
+GROUP BY NCC.maNCC
+
+--26. Mỗi nhà cung cấp vận chuyển bao nhiêu phụ tùng? Hiển thị mã nhà cung 
+--cấp, tên, thành phố của nhà cung cấp và tổng số phụ tùng đã vận chuyển
+SELECT NCC.maNCC, tenNCC, NCC.thanhpho, SUM(soluong) 'SL_PT'
+FROM Vanchuyen VC RIGHT JOIN Nhacungcap NCC ON VC.MANCC=NCC.MANCC
+GROUP BY NCC.maNCC, tenNCC,thanhpho
+
+--27. Nhà cung cấp nào đã vận chuyển tổng cộng nhiều hơn 500 phụ tùng? Chỉ
+--hiển thị mã nhà cung cấp
+SELECT VC.maNCC, SUM(soluong) 'SL_PT'
+FROM Vanchuyen VC 
+GROUP BY VC.maNCC
+HAVING SUM(SOLUONG)>500
+
+--28. Nhà cung cấp nào đã vận chuyển nhiều hơn 300 phụ tùng màu đỏ (red). 
+--Chỉ hiển thị mã nhà cung cấp.
+SELECT VC.maNCC, SUM(soluong) 'SL_PT'
+FROM Vanchuyen VC JOIN PHUTUNG PT ON VC.maPT= PT.maPT
+WHERE mausac='RED'
+GROUP BY VC.maNCC
+HAVING SUM(SOLUONG)>300
+
+--29. Nhà cung cấp nào đã vận chuyển nhiều hơn 300 phụ tùng màu đỏ (red). 
+--Hiển thị mã nhà cung cấp, tên, thành phố và số lượng phụ tùng màu đỏ đã 
+--vận chuyển.
+SELECT NCC.maNCC, tenNCC, NCC.thanhpho, SUM(soluong) 'SL_PT'
+FROM (Vanchuyen VC JOIN PHUTUNG PT ON VC.maPT= PT.maPT)JOIN Nhacungcap NCC ON NCC.MANCC=VC.MANCC
+WHERE mausac='RED'
+GROUP BY NCC.maNCC, tenNCC, NCC.thanhpho
+HAVING SUM(SOLUONG)>300
+
+--30. Có bao nhiêu nhà cung cấp ở mỗi thành phố.
+SELECT thanhpho , COUNT(MANCC) NHACUNGCAP
+FROM Nhacungcap
+GROUP BY thanhpho
+
+--31. Nhà cung cấp nào đã vận chuyển nhiều phụ tùng nhất. Hiển thị tên nhà
+--cung cấp và số lượng phụ tùng đã vận chuyển.
+SELECT TOP 1 TENNCC, SUM(SOLUONG) SOLUONG
+FROM NHACUNGCAP A JOIN VANCHUYEN B ON A.MANCC= B.MANCC 
+GROUP BY TENNCC
+ORDER BY SUM(SOLUONG) DESC
+
+--32. Thành phố nào có cả nhà cung cấp và phụ tùng.
+SELECT THANHPHO
+FROM Nhacungcap
+INTERSECT 
+SELECT THANHPHO
+FROM Phutung
+
+--33. Viết câu lệnh SQL để insert nhà cung cấp mới: S6, Duncan, 30, Paris.
+INSERT inTO NHACUNGCAP VALUES('S6', 'Duncan' , '30' , 'Paris')
+
+--34. Viết câu lệnh SQL để thay đổi thanh phố S6 (ở câu 33) thành Sydney.
+update
+Nhacungcap
+set thanhpho= 'Sydney'
+WHERE thanhpho = 'Ducan'
+
+--35. Viết câu lệnh SQL tăng trangthai của nhà cung cấp ở London lên thêm
+update 
+Nhacungcap 
+set trangthai = trangthai + '10'
+where Nhacungcap.thanhpho = 'london'
+
+--36. Viết câu lệnh SQL xoá nhà cung cấp S6
+delete from Nhacungcap
+where Nhacungcap.maNCC ='S6'
